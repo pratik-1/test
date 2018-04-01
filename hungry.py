398f152efd4d12ee763c48e32b7665f2795ffe21
pratik-1 <nandekarpratik04@gmail.com>
2018-04-01 20:00:41 +0530

    add thirsty

--- hungry.py
+++ hungry.py
@@ -4,4 +4,6 @@ if hungry=='yes':
     print('eat pizza')
     print('dosa')
 else:
-    print('do your wrk')
+    thirsty = input('are you thirsty')
+    if thirsty=='yes':
+        print('drink water')
